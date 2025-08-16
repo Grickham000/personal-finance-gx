d216dbe8fbf87a79cef47c925da66b02cf9011bc
jose-alejandrodiaz-by <guru.aalfa@gmail.com>
2025-08-16 15:43:23 -0600

    Adding useful descriptions to the the requirements.txt Remember to update the web api key env variable and get the private key it is used in the firebaseconfig.

--- Common/firebase_config.py
+++ Common/firebase_config.py
@@ -2,5 +2,5 @@ import firebase_admin
 from firebase_admin import credentials
 
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate('personalfinance-a0728-firebase-adminsdk-vr1aa-826b24ddc7.json')
+cred = credentials.Certificate('personalfinance-a0728-firebase-adminsdk-vr1aa-1b8d2c9ed3.json')
 firebase_app = firebase_admin.initialize_app(cred)
