7a8a85f07a16a64968851dfd5e4253ef474b39b0
Grickham000 <71575244+Grickham000@users.noreply.github.com>
2026-07-01 22:14:13 -0600

    fix(api): change auth level to ANONYMOUS to support client connections

--- function_app.py
+++ function_app.py
@@ -3,4 +3,4 @@ from Common.firebase_config import firebase_app  # Ensure Firebase is initialize
 from API.fastapi_app import fastapi_app
 
 # Expose the FastAPI app through Azure Functions ASGI wrapper
-app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.FUNCTION)
+app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
